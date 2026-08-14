20d73de50300acc366852773c8955821d3968f95
bmillerx64 <bmillerx64@gmail.com>
2026-08-14 15:18:39 -0500

    GS254: test native provider row flags directly

--- tests/test_254_provider_rows.py
+++ tests/test_254_provider_rows.py
@@ -2,4 +2,4 @@ import mide.webull_connection as connection
 
 
 def test_every_native_pipeline_row_marks_alpaca_unused():
-    assert all(row["Alpaca used"] == "No" for row in connection._webull_native_pipeline_sources(object()))
+    assert all(row["Alpaca used"] == "No" for row in connection._native_pipeline_rows())
