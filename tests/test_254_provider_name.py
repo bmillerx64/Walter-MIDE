e5e92ce440c739e33f366082c109446f800f0ad6
bmillerx64 <bmillerx64@gmail.com>
2026-08-14 15:18:15 -0500

    GS254: test native provider rows directly

--- tests/test_254_provider_name.py
+++ tests/test_254_provider_name.py
@@ -2,5 +2,5 @@ import mide.webull_connection as connection
 
 
 def test_native_pipeline_names_webull_as_universe_provider():
-    rows = connection._webull_native_pipeline_sources(object())
+    rows = connection._native_pipeline_rows()
     assert rows[0]["Actual provider"] == "Webull OpenAPI SDK"
