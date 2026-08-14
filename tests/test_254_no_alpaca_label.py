a35ab5b3ec66c38ac380ac94aca05152d8981849
bmillerx64 <bmillerx64@gmail.com>
2026-08-14 15:17:56 -0500

    GS254: test active native provenance rows directly

--- tests/test_254_no_alpaca_label.py
+++ tests/test_254_no_alpaca_label.py
@@ -2,5 +2,5 @@ import mide.webull_connection as connection
 
 
 def test_native_pipeline_contains_no_alpaca_provider_name():
-    text = repr(connection._webull_native_pipeline_sources(object()))
+    text = repr(connection._native_pipeline_rows())
     assert "Alpaca Trading API" not in text
