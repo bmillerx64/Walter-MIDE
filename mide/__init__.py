6a1089115198f7f32a8648d4110b0ce3a0745b0a
bmillerx64 <bmillerx64@gmail.com>
2026-09-01 11:37:09 -0500

    GS347 install native radar runtime guard

--- mide/__init__.py
+++ mide/__init__.py
@@ -192,3 +192,7 @@ _install_gs344_emergence_convergence_engine()
 from .gs345_persistent_leader_escalation import install as _install_gs345_persistent_leader_escalation  # noqa: E402
 
 _install_gs345_persistent_leader_escalation()
+
+from .gs347_native_radar_timeout_health import install as _install_gs347_native_radar_timeout_health  # noqa: E402
+
+_install_gs347_native_radar_timeout_health()
